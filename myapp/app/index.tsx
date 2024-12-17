9a3ba11d46ed45039ade7f44562dd9b407143f4f
UserLasa73 <lasanthakumaraofficial@gmail.com>
2024-12-17 23:48:00 +0530

    reset the project and remove default files

---
+++ myapp/app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View, Image} from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+
+      <Image source={require('../assets/images/react-logo.png')}/>
+
+      <Text>Hello World</Text>
+    </View>
+  );
+}
